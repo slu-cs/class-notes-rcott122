05694b32e9f3bf59ed3b6017c170696662e72976
Lisa Torrey <ltorrey@stlawu.edu>
2020-02-03 13:07:10 -0500

    JavaScript object literals

--- javascript/objects.js
+++ javascript/objects.js
@@ -3,3 +3,23 @@
 // Object literal
 const point = {x: 1, y: 2};
 console.log(point);
+
+// Accessing properties
+console.log(point.x);
+
+// Changing properties
+point.x = 3;
+
+// Nested object literal
+const circle = {
+  center: point,
+  radius: 4
+};
+
+// Accessing subproperties
+console.log(circle.center.x);
+
+// Undefined subproperties
+console.log(circle.color);
+circle.color = 'blue';
+console.log(circle);
