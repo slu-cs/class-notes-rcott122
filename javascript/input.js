23fd3698c946f5d5bd9288d3e5fcdac79c5f2689
Riley Cott <rcott@Rileys-MacBook-Pro-3.local>
2020-02-05 14:21:34 -0500

    input

--- javascript/input.js
+++ javascript/input.js
@@ -1,5 +1,5 @@
 //syntax for console and file input
-
+const fs = require('fs');
 const readline = require('readline');
 
 //console configuration
@@ -10,5 +10,20 @@ const user = readline.createInterface({
 
 //console input
 user.question('Filename: ', function(filename) {
-  console.log(filename);
+
+  //file configuration
+  const file = readline.createInterface({
+    input: fs.createReadStream(filename);
+  });
+
+  //asycnchronous line by line input
+  file.on('line', function(line) {
+    console.log(line);
+  });
+
+  //end the program when the file closes
+  file.on('close', function() {
+    process.exit(0);
+  });
+
 });
