5a608328535156780993f1cc46e43a26ce68b4d9
Riley Cott <rcott@Rileys-MacBook-Pro-3.local>
2020-02-10 13:19:08 -0500

    mongo

---
+++ mongo/faculty/db.js
@@ -0,0 +1,21 @@
+//connect to a database about faculty
+
+const mongoose = require('mongoose');
+
+//export  this function
+module.exports = function() {
+
+  //avoid warnings
+  mongoose.set('useNewUrlParser', true);
+ mongoose.set('useUnifiedTopology', true);
+ mongoose.set('useCreateIndex', true);
+ mongoose.set('useFindAndModify', false);
+
+ // Start connecting
+ mongoose.connect('mongodb://localhost/faculty');
+
+ // Make sure we see any errors
+ mongoose.connection.on('error', function(error) {
+   console.error(error.stack);
+ });
+};
