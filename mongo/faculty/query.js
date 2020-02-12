017391f84014d8e75da08ebafa9752b99a511fc6
Riley Cott <rcott@Rileys-MacBook-Pro-3.local>
2020-02-12 13:24:47 -0500

    more queries

--- mongo/faculty/query.js
+++ mongo/faculty/query.js
@@ -7,8 +7,64 @@ const Professor = require('./schema');
 connect(); // To the database
 
 //what documents are in the collection
-const query = Professor.find();
-query.exec(function(error, professors){
+//const query = Professor.find();
+//query.exec(function(error, professors){
+  //if (error) console.error(error.stack);
+  //console.log(professors);
+//});
+
+const queries = [
+
+  //what are the names in alaphabetical order?
+  Professor.find().sort ('name'),
+
+  //who started modt recently?
+  Professor.find().sort('-started').limit(1),
+
+  //who started in 2003?
+  Professor.find().where('started').equals(2003),
+
+  //who teaches 362?
+  Professor.find().where('courses').in(362),
+
+  //what are all the ranks?
+  Professor.distinct('rank')
+];
+
+queries[0].exec(function(error, professors){
   if (error) console.error(error.stack);
-  console.log(professors);
+
+  const names = professors.map(p => p.name);
+  console.log('Names in order: ', names);
+});
+
+// Professor.find().sort('-started').limit(1)
+queries[1].exec(function(error, professors) {
+  if (error) console.error(error.stack);
+
+  const names = professors.map(p => p.name);
+  console.log('Started most recently: ', names);
+});
+
+// Professor.find().where('started').equals(2003)
+queries[2].exec(function(error, professors) {
+  if (error) console.error(error.stack);
+
+  const names = professors.map(p => p.name);
+  console.log('Started in 2003: ', names);
+});
+
+// Professor.find().where('courses').in(362)
+queries[3].exec(function(error, professors) {
+  if (error) console.error(error.stack);
+
+  const names = professors.map(p => p.name);
+  console.log('Teaches 362: ', names);
+});
+
+// Professor.distinct('rank')
+queries[4].exec(function(error, ranks) {
+  if (error) console.error(error.stack);
+
+  console.log('Distinct ranks: ', ranks);
 });
