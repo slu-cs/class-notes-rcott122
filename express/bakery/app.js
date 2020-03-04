59fd1d50419dd667c9a3483293891c9d1b17add3
Riley Cott <rcott@Rileys-MacBook-Pro-3.local>
2020-03-04 13:57:52 -0500

    j

--- express/bakery/app.js
+++ express/bakery/app.js
@@ -30,7 +30,7 @@ app.use(function(request, response, next){
 });
 
 //handle other errors
-app.use(error, request, response, next){
+app.use(function(error, request, response, next){
   console.error(error.stack);
   response.status(500).send(error.message);
 });
