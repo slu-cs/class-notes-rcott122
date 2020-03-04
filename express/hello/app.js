bb80ff85e5b231fc6124b74c099942a6082df54d
Riley Cott <rcott@Rileys-MacBook-Pro-3.local>
2020-03-04 13:35:39 -0500

    changes

--- express/hello/app.js
+++ express/hello/app.js
@@ -5,8 +5,29 @@ const express = require('express');
 const app = express();
 
 //respond to requests
+//cs-linuxlab-12.stlawu.edu:3000/
 app.get('/', function(request, response){
-  response.send('Hello Lisa');
+  response.send('Hello Riley');
+});
+
+//cs-linuxlab-12.stlawu.edu:3000/foo
+app.get('/foo', function(request, response){
+  response.send('Hello foo');
+});
+
+//cs-linuxlab-12.stlawu.edu:3000/foo/bar
+app.get('/foo/bar', function(request, response){
+  response.send('Hello bar');
+});
+
+//cs-linuxlab-12.stlawu.edu:3000/zap/:z (for any z)
+app.get('/zap/:z', function(request, response){
+  response.send('Hello ${request.params.z}');
+});
+
+//cs-linuxlab-12.stlawu.edu:3000/zap?x
+app.get('/zap', function(request, response){
+  response.send('Hello zap with ${request.query.x} and ${request.query.y}');
 });
 
 //start the server
