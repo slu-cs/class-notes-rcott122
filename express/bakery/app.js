82a1a45ca2142c674f2b8bbd94c4b8a116ab716d
Riley Cott <rcott@Rileys-MacBook-Pro-3.local>
2020-03-04 13:54:31 -0500

    bakery

--- express/bakery/app.js
+++ express/bakery/app.js
@@ -5,11 +5,35 @@ const express = require('express');
 const app = express();
 
 //respond to requests
-//cs-linuxlab-12.stlawu.edu:3000/
-app.get('/', function(request, response){
-  response.send('Hello Riley');
+app.use(function(request, response, next){
+  console.log('---------------', new Date().toLocateString());
+  console.log(request.method, request.url);
+  console.log('Body =', request.body);
+  next(); // keeps handling this request
 });
 
+//give this app a homepage
+app.get('/', function(request, response){
+  response.send(`
+    <h1>Bakery</h1>
+    <ul>
+      <li><a href="/cakes">Cakes</a></li>
+      <li><a href="/pies">Pies</a></li>
+    </ul>
+  `);
+});
+
+//handle undefined routes (middle ware)
+app.use(function(request, response, next){
+  console.log('Replied with 404');
+  response.status(404).end();
+});
+
+//handle other errors
+app.use(error, request, response, next){
+  console.error(error.stack);
+  response.status(500).send(error.message);
+});
 
 //start the server
 app.listen(3000); //port address
